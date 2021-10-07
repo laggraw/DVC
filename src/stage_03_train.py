bf82363a6afa535bed928da2293831b875421a8a
Lalit Aggarwal <agg.lalit@gmail.com>
2021-10-07 23:13:43 +0530

    stage 03 train and model creation

--- src/stage_03_train.py
+++ src/stage_03_train.py
@@ -5,6 +5,7 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet,LinearRegression
+import joblib
 
 
 def train_model(config_path, params_path):
@@ -28,11 +29,26 @@ def train_model(config_path, params_path):
     train_y = train_data['quality']
     train_x = train_data.drop('quality', axis=1)
 
+    model_dir = config["artifacts"]["model_dir"]
+    En_model_filename = config["artifacts"]["En_model_file"]
+    Lr_model_filename = config["artifacts"]["Lr_model_file"]
+
+
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    En_model_file_path = os.path.join(artifacts_dir, model_dir, En_model_filename)
+    Lr_model_file_path = os.path.join(artifacts_dir, model_dir, Lr_model_filename)
+    create_directory(dirs =[model_dir_path])
+ 
+
     en = ElasticNet(alpha=alpha_value, l1_ratio= l1_ratio_value, random_state=random_state)
     en.fit(train_x, train_y)
+    joblib.dump(en,En_model_file_path)
 
     lr = LinearRegression()
     lr.fit(train_x, train_y)
+
+    joblib.dump(lr,Lr_model_file_path)
+
     print("training done")
 
 if __name__ =='__main__':
